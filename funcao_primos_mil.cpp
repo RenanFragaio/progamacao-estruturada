bdd6e2c860c518a5e7a4afbbf25f7fd43f48c766
RENAN FRAGAIO <2017200405@UC.ACAD>
2018-03-15 08:19:03 -0300

    My first git file

---
+++ funcao_primos_mil.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Todos os números Primos de 1 até 1000
+void Primo(){
+	int i;
+	for(i=1;i<=1000;i++){
+		if((i%2==0)||(i%3==0)||(i%5==0)||(i%7==0)){
+			printf("%d Não é Primo \n",i);
+		}
+		else{
+			printf("%d é Primo \n",i);
+		}
+	}
+}
+
+int main(){
+	Primo();
+}
