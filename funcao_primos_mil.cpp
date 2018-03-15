f0c1c062a4ac1eae2a8a97a4b377efa150fdd7ff
lepfalt <36522967+lepfalt@users.noreply.github.com>
2018-03-15 08:45:57 -0300

    Update funcao_primos_mil.cpp

--- funcao_primos_mil.cpp
+++ funcao_primos_mil.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-//Todos os números Primos de 1 até 1000
+//Todos os nÃºmeros Primos de 1 atÃ© 1000
 void Primo(){
 	int i;
 	for(i=1;i<=1000;i++){
 		if((i%2==0)||(i%3==0)||(i%5==0)||(i%7==0)){
-			printf("%d Não é Primo \n",i);
+			printf("%d NÃ£o Ã© Primo !! \n",i);
 		}
 		else{
-			printf("%d é Primo \n",i);
+			printf("%d Ã© Primo !! \n",i);
 		}
 	}
 }
